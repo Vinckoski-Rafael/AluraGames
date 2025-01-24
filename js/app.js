64325e062ca3859044812b5b3b7c28f86e79d6ce
Rafael Vinckoski Roque <rafael_v_roque@hotmail.com>
2025-01-24 19:55:03 -0300

    Criaçao da funcionalidade de troca de texto conforme mudança de status do jogo

--- js/app.js
+++ js/app.js
@@ -4,7 +4,17 @@ function alterarStatus(id){
     let botao=jogoEscolhido.querySelector(".dashboard__item__button");
 
     if(imagem){
-    imagem.classList.toggle("dashboard__item__img--rented");
+        imagem.classList.toggle("dashboard__item__img--rented");
+    }
+    
+    if(botao){
+        botao.classList.toggle("dashboard__item__button--return");
+        if(botao.classList.contains("dashboard__item__button--return")){
+            botao.textContent="devolver";
+        }
+        else{
+            botao.textContent="Alugar";
+        }       
     }
     else{
         return;
